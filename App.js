059f63a7dddc4866afa366914e3b5772dcdbef6a
Elvis Agbesi <elvisagbesi@gmail.com>
2021-03-26 03:15:27 +0000

    feat: :construction: App working in the most basic way possible

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql } from '@apollo/client';
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Dimensions, FlatList, RefreshControl, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, FlatList, Image, RefreshControl, SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
 
 
 const CHARACTERS = gql`
 query{
   characters{
     results{
+      id
       name
-      species
+      status
+      image
     }
   }
 }
@@ -21,20 +22,46 @@ const client = new ApolloClient({
 });
 
 const Item = ({ data }) => (
-  <View style={{flexDirection:'column', marginVertical: 5}}>
-    <Text>{data.name}</Text>
-    <Text>{data.species}</Text>
+  <View style={{ flexDirection: 'row', marginVertical: 5 }}>
+
+    <View style={{ position: 'relative' }}>
+      {
+        (data.status.toLowerCase() === "dead") ?
+          <View style={{ height: 50, width: 50, backgroundColor: 'rgba(255,255,255,0.5)', position: 'absolute', top: 0, left: 0, zIndex: '900' }} />
+          :
+          null
+      }
+      <Image
+        source={{
+          uri: data.image
+        }}
+
+        style={{
+          borderRadius: 25,
+          height: 50,
+          width: 50
+        }}
+      />
+    </View>
+
+    <View style={{ justifyContent: 'space-around', marginLeft: 10 }}>
+      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{data.name}</Text>
+      <Text style={{ color: `${(data.status.toLowerCase() === "dead") ? 'red' : 'black'}` }}>{data.status}</Text>
+    </View>
+
   </View>
 );
 
 function CharacterList() {
 
-  const [list , setList] = useState([]);
-  const [loader,setloader] = useState(true)
-  const { loading, error, data } = useQuery(CHARACTERS, {onCompleted: d => {
-    setList(d?.characters.results);
-    setloader(false)
-  },onError: e => console.log(JSON.stringify(e,null,2))})
+  const [list, setList] = useState([]);
+  const [loader, setloader] = useState(true)
+  const { loading, error, data } = useQuery(CHARACTERS, {
+    onCompleted: d => {
+      setList(d?.characters.results);
+      setloader(false)
+    }, onError: e => console.log(JSON.stringify(e, null, 2))
+  })
 
   const renderItem = ({ item }) => (
     <Item data={item} />
@@ -45,18 +72,23 @@ function CharacterList() {
   return (
     <FlatList
       data={list}
+      showsVerticalScrollIndicator={false}
       renderItem={renderItem}
-      refreshControl={<RefreshControl refreshing={loader}/>}
+      refreshControl={<RefreshControl refreshing={loader} />}
     />
   )
 }
 
 export default function App() {
 
+  StatusBar.setBarStyle("dark-content")
+
   return (
     <ApolloProvider client={client}>
       <SafeAreaView style={{ height: Dimensions.get('window').height, }}>
-        <CharacterList />
+        <View style={{ paddingHorizontal: 15 }}>
+          <CharacterList />
+        </View>
       </SafeAreaView>
     </ApolloProvider>
   );
